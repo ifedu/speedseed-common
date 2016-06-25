42f6b72022eef06581c6919ea5351f284f99cf8b
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2016-06-26 01:34:40 +0200

    feat: getters and setters yeoman

--- _Yo.js
+++ _Yo.js
@@ -5,6 +5,31 @@ module.exports = class Yo extends generators.Base {
         super(...args)
     }
 
+    configGet() {
+        const $ = {}
+
+        for (let option in this.options) {
+            $[option] = this.config.get(option)
+        }
+
+        return $
+    }
+
+    configSet() {
+        for (let option in this.options) {
+            if (
+                option === 'env' ||
+                option === 'resolved' ||
+                option === 'argv' ||
+                option === 'namespace' ||
+                option === 'skip-cache' ||
+                option === 'skip-install'
+            ) continue
+
+            this.config.set(option, this.options[option])
+        }
+    }
+
     create(fileTpl, fileDest, option) {
         const copy = (option === false) ? 'copy' : 'copyTpl'
         const options = (option === false) ? { globOptions: { dot: true } } : this.config.getAll()
