7f9ca541c1ec730e759baeac30891c79aacb21df
Edu Pérez-Bermejo <ifedu@outlook.com>
2017-09-18 20:52:57 +0200

    v.0.22.4

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const tsConfigPaths = require('tsconfig-paths')
 const tsConfig = require('./tsconfig.json')
 
 tsNode.register({
+    fast: true,
+
     compilerOptions: {
         allowJs: true
     },
-    ignore: [],
+
+    ignore: [
+        /^(.(?!speedseed))*$/
+    ],
 
     project: path.resolve(__dirname),
 })
